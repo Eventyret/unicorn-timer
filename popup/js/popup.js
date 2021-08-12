b51a4e3867fc023181367a7212b6682216326534
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-12 10:47:51 +0100

    Added delete and add

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,14 +1,27 @@
 const addTaskRef = document.querySelector('#add-task');
 const taskContainerRef = document.querySelector('#task-container');
 
+const tasks = [];
+
 const addTask = () => {
+  const taskNum = tasks.length;
+  tasks.push('');
   const taskRow = document.createElement('div');
-  const deleteBtn = document.createElement('input');
   const text = document.createElement('input');
   text.type = 'text';
   text.placeholder = 'Enter a task...';
+
+  text.addEventListener('change', () => {
+    tasks[taskNum] = text.value;
+    console.log(tasks);
+  });
+
+  const deleteBtn = document.createElement('input');
   deleteBtn.type = 'button';
   deleteBtn.value = 'X';
+  deleteBtn.addEventListener('click', () => {
+    taskk.splice(taskNum, 1);
+  });
 
   taskRow.appendChild(text);
   taskRow.appendChild(deleteBtn);
