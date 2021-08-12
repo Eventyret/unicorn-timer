b4f76a4685bd3cd9a9e8ea93707879e89115434d
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-12 15:44:16 +0100

    Added padding to numbers

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -88,12 +88,13 @@ const renderTasks = () => {
 
 const updateTime = () => {
   chrome.storage.local.get(['timer'], (res) => {
-    const minutes = 25 - Math.ceil(res.timer / 60);
+    res.timer = 60 * 24;
+    const minutes = `${25 - Math.ceil(res.timer / 60)}`.padStart(2, '0');
     let seconds = '00';
     if (res.timer % 60 !== 0) {
-      seconds = 60 - (res.timer % 60);
+      seconds = `${60 - (res.timer % 60)}`.padStart(2, '0');
     }
-    timeRef.textContent = `${minutes}: ${seconds}`;
+    timeRef.textContent = `${minutes}:${seconds}`;
   });
 };
 
