98dd4523f1f8c4ab028acc1e56a6810d5d02c12f
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-12 13:22:38 +0100

    Fixed timer not working

--- background.js
+++ background.js
@@ -1,18 +1,22 @@
-chrome.alarms.create({ periodInMinutes: 1 / 60 });
+chrome.alarms.create('unicornTimer', {
+  periodInMinutes: 1 / 60
+});
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'unicornTimer') {
     chrome.storage.local.get(['timer', 'isRunning'], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
+        console.log(timer);
         chrome.storage.local.set({ timer });
       }
     });
   }
-  chrome.storage.local.get(['timer', 'isRunning'], (res) => {
-    chrome.storage.local.set({
-      timer: 'timer' in res ? res.timer : 0,
-      isRunning: 'isRunning' in res ? res.isRunning : false
-    });
+});
+
+chrome.storage.local.get(['timer', 'isRunning'], (res) => {
+  chrome.storage.local.set({
+    timer: 'timer' in res ? res.timer : 0,
+    isRunning: 'isRunning' in res ? res.isRunning : false
   });
 });
