367a26c24c2add5a682e97ea3b8fe2ce57ec9b04
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-12 10:43:49 +0100

    Added task loop

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,32 +1,18 @@
-const timeRef = document.querySelector('#time');
-const nameRef = document.querySelector('#name');
-const timerRef = document.querySelector('#timer');
-const startBtnRef = document.querySelector('#start-timer');
-const stopBtnRef = document.querySelector('#stop-timer');
-const resetBtnRef = document.querySelector('#reset-timer');
+const addTaskRef = document.querySelector('#add-task');
+const taskContainerRef = document.querySelector('#task-container');
 
-function updateTimeElements() {
-  const currentTime = new Date().toLocaleTimeString();
-  chrome.storage.local.get(['timer'], (res) => {
-    const time = res.timer ?? 0;
-    timerRef.textContent = `The timer is at: ${time} seconds`;
-  });
-  timeRef.innerHTML = `The time is ${currentTime}`;
-}
-updateTimeElements();
-setInterval(updateTimeElements, 1000);
+const addTask = () => {
+  const taskRow = document.createElement('div');
+  const deleteBtn = document.createElement('input');
+  const text = document.createElement('input');
+  text.type = 'text';
+  text.placeholder = 'Enter a task...';
+  deleteBtn.type = 'button';
+  deleteBtn.value = 'X';
 
-chrome.storage.sync.get(['name'], (items) => {
-  const name = items.name ?? 'Unknown';
-  nameRef.textContent = `Your name is ${name}`;
-});
+  taskRow.appendChild(text);
+  taskRow.appendChild(deleteBtn);
+  taskContainerRef.appendChild(taskRow);
+};
 
-startBtnRef.addEventListener('click', () => {
-  chrome.storage.local.set({ isRunning: true });
-});
-stopBtnRef.addEventListener('click', () => {
-  chrome.storage.local.set({ isRunning: false });
-});
-resetBtnRef.addEventListener('click', () => {
-  chrome.storage.local.set({ isRunning: false, timer: 0 });
-});
+addTaskRef.addEventListener('click', () => addTask());
