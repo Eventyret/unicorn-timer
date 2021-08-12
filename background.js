1df489f0e593cd125338cf233914a3cba11b8991
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-12 12:56:47 +0100

    Started working on service worker

--- background.js
+++ background.js
@@ -1,21 +1,18 @@
 chrome.alarms.create({ periodInMinutes: 1 / 60 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  chrome.storage.local.get(['timer', 'isRunning'], (res) => {
-    const time = res.timer ?? 0;
-    const isRunning = res.isRunning ?? true;
-    if (!isRunning) return;
-    chrome.storage.local.set({ timer: time + 1 });
-    chrome.action.setBadgeText({ text: `${time + 1}` });
-
-    chrome.storage.sync.get(['notificationTime'], (res) => {
-      const notificationTime = res.notificationTime ?? 1000;
-      if (time % notificationTime === 0) {
-        this.registration.showNotification('Unicorn Timer', {
-          body: `${notificationTime} second has passed`,
-          icon: 'assets/images/icon.png'
-        });
+  if (alarm.name === 'unicornTimer') {
+    chrome.storage.local.get(['timer', 'isRunning'], (res) => {
+      if (res.isRunning) {
+        let timer = res.timer + 1;
+        chrome.storage.local.set({ timer });
       }
+    });
+  }
+  chrome.storage.local.get(['timer', 'isRunning'], (res) => {
+    chrome.storage.local.set({
+      timer: 'timer' in res ? res.timer : 0,
+      isRunning: 'isRunning' in res ? res.isRunning : false
     });
   });
 });
