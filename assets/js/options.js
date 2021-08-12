e7b2f3a30ae371d8dfc8938d1905c446293e6e36
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-12 10:04:42 +0100

    Adding notification time to options

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,13 +1,16 @@
 const nameInputRef = document.querySelector('#name-input');
+const timeInputRef = document.querySelector('#time-input');
 const saveBtnRef = document.querySelector('#save-btn');
 
 saveBtnRef.addEventListener('click', () => {
   const name = nameInputRef.value;
+  const notificationTime = timeInputRef.value;
   chrome.storage.sync.set({ name }, () => {
     console.log(`Name is set to ${name}`);
   });
 });
 
-chrome.storage.sync.get(['name', 'test'], (items) => {
-  nameInputRef.value = items.name;
+chrome.storage.sync.get(['name', 'notificationTime'], (items) => {
+  nameInputRef.value = items.name ?? '';
+  timeInputRef.value = item.notificationTime ?? 1000;
 });
